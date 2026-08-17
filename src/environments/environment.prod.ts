a58c3e4554a94c331bd09733616cfc9ee30e9e95
Hanna Katai <hanna01.katai@gmail.com>
2026-08-17 16:06:38 +0300

    Connect live Firebase Realtime Database URL for global multi-device sync

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   // Live Official Firebase Realtime Database for AGROMEC SFÂNTU GHEORGHE SA
   firebase: {
     databaseURL: 'https://agromec-website-default-rtdb.europe-west1.firebasedatabase.app'
